14f58b601c74699b2e4e6c50cd361bc3bab92416
annamelya2021 <anna.basyuk@gmail.com>
2024-05-24 09:54:03 +0200

    readme

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import userController  from "../controllers/users/userController.js";
-import userModel from '../models/userModel.js';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -35,62 +34,63 @@ const isAuthenticated = async(req,res,next)=>{
 
 
 
-// const isAuthenticated = async (req, res, next) => {
-//   const authorization = req.headers.authorization;
-//   if (!authorization) {
-//     return res.status(401).json({ error: "Token JWT is missing" });
-//   }
-
-//   try {
-//     const token = authorization.split("Bearer ")[1];
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     // const user = await userModel.findById(decoded._id);
-//     const user = await userModel.findOne(decoded._id);
-//     console.log(decoded)
-    
-
-//     if (!user) {
-//       return res.status(400).json({ error: "User does not exist" });
-//     }
-
-//     req.user = user;
-//     next();
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ error: "An error occurred" });
-//   }
-// }
 
 export default isAuthenticated;
 
 
 
-const isAdmin = async(req,res,next)=>{
-    const authorization  =req.headers.authorization;
-    if(!authorization){
-        return res.status(401).json({error:"no hay token jwt"});
-    }
-    try {
-        const token = authorization.split("Bearer ")[1];
-        const decoded =jwt.verify(token,process.env.JWT_SECRET);
-        const user = await userController.getById(decoded._id);
-        if(!user){
-            return res.status(400).json({error:"No existe el usurio"});
-        }
-        if(user.role !== "admin"){
-            return res.status(401).json({error:"No estás autorizado"});
-        }
-        req.user = user;
-        next();
+// const isAdmin = async(req,res,next)=>{
+//     const authorization  =req.headers.authorization;
+//     if(!authorization){
+//         return res.status(401).json({error:"doesn't have a token jwt"});
+//     }
+//     try {
+//         const token = authorization.split("Bearer ")[1];
+//         const decoded =jwt.verify(token,process.env.JWT_SECRET);
+//         const user = await userController.getById(decoded._id);
+//         if(!user){
+//             return res.status(400).json({error:"user does not exist"});
+//         }
+//         if(user.role !== "admin"){
+//             return res.status(401).json({error:"does have a permissions as admin"});
+//         }
+//         req.user = user;
+//         next();
         
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({error:"ha habido un error"});
+//     } catch (error) {
+//         console.error(error);
+//         return res.status(500).json({error:"An error ocurred"});
+//     }
+
+// }
+
+
+const isAdmin = async (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).json({ error: "Token JWT is missing" });
     }
+  
+    try {
+      const token = authorization.split("Bearer ")[1];
+      const decoded = jwt.verify(token, SECRET_TOKEN_KEY);
+      const user = await userController.getById(decoded._id);
+  
+      if (!user) {
+        return res.status(400).json({ error: "User does not exist" });
+      }
+  
+      if (user.role !== "admin") {
+        return res.status(403).json({ error: "Insufficient permissions" });
+      }
+  
+      req.user = user;
+      next();
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "An error occurred" });
+    }
+  }
+  
+  export { isAuthenticated, isAdmin };
 
-}
-
-export {
-    isAuthenticated,
-    isAdmin
-}
--- src/router/post-routes.js
+++ src/router/post-routes.js
@@ -1,7 +1,7 @@
 import ctrlWrapper from "../helpers/ctrlWrapper.js";
 import express from 'express';
 import allPosts from '../controllers/posts/apiPostsController.js';
-import { isAuthenticated } from "../middlewares/authMiddleware.js";
+import { isAuthenticated ,isAdmin} from "../middlewares/authMiddleware.js";
 
 
 const router = express.Router();
@@ -9,7 +9,8 @@ const router = express.Router();
 router.get('/', ctrlWrapper(allPosts.getAllPosts));
 router.post('/', isAuthenticated, ctrlWrapper(allPosts.createPost));
 router.get('/:id',ctrlWrapper(allPosts.getOnePost));
-router.delete('/:id',isAuthenticated, ctrlWrapper( allPosts.deletePost));
+// router.delete('/:id',isAuthenticated, ctrlWrapper( allPosts.deletePost));
+router.delete('/:id',isAuthenticated, isAdmin, ctrlWrapper( allPosts.deletePost));
 router.put('/:id',isAuthenticated, ctrlWrapper(allPosts.updatePost));
 
 export default router;
