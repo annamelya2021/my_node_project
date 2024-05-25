6b2154145ba90d1225b8920d92fa44121d799d24
annamelya2021 <anna.basyuk@gmail.com>
2024-05-25 19:43:34 +0200

    commets roles

--- src/router/comment-routes.js
+++ src/router/comment-routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import commentControllers from '../controllers/comments/apiCommentController.js';
 import ctrlWrapper from '../helpers/ctrlWrapper.js';
+import {isAuthenticated, isAdmin } from '../middlewares/authMiddleware.js';
 
 
 const router = Router();
 
-router.post('/', ctrlWrapper(commentControllers.createComment));
-router.get('/:postId', ctrlWrapper(commentControllers.getCommentsByPostId));
-router.delete('/:id', ctrlWrapper(commentControllers.deleteComment));
+router.post('/', isAuthenticated, ctrlWrapper(commentControllers.createComment));
+router.get('/:postId', isAuthenticated, ctrlWrapper(commentControllers.getCommentsByPostId));
+router.delete('/:id', isAuthenticated, isAdmin, ctrlWrapper(commentControllers.deleteComment));
 
 export default router;
